a0b5045e30efd325dadd6bfcef4d3324a38a8fb7
mdarora <mdarora0001@gmail.com>
2021-08-25 14:05:32 +0530

    user schema for database

---
+++ db/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+},{ timestamps : true });
+
+module.exports = new mongoose.model('User', userSchema);
