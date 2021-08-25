730be956eccb9b3aa1486bfa431c0528cb6fa184
mdarora <mdarora0001@gmail.com>
2021-08-25 13:13:08 +0530

    installed dotenv & configered port as env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("listening on port : 3000");
+require('dotenv').config();
+
+app.listen(process.env.PORT, () =>{
+    console.log("listening on port : " + process.env.PORT);
 })
