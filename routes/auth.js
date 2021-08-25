2e599cabb4e201353f005bd4e3ed6ddfc350eda2
mdarora <mdarora0001@gmail.com>
2021-08-25 18:56:47 +0530

    hashed password with bcryptjs and completed register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const User = require('../db/models/user');
 
@@ -16,12 +17,19 @@ router.post("/register", async (req, res)=>{
         if (findByUsername.length !== 0){
             return res.json({error: "Username already exist."});
         }
-        return res.json({message: findByUsername});
+
+        const hashedpass = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            name, username,
+            password: hashedpass
+        });
+        await newUser.save();
+        return res.json({message: "Successfully registered", newUser});
+
     } catch (error) {
+        console.log("Catched on register: ", error);
         return res.json({error: "Something went wrong!"})
     }
-
-    return res.send("register here");
 });
 
 module.exports = router;
