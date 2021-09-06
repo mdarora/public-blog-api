620c2ad183f0991c99c12c17d82fec6cf9914453
mdarora <mdarora0001@gmail.com>
2021-09-06 18:59:11 +0530

    login authentication

---
+++ middleware/loginAuth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const loginAuth = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({error: "Login first"});
+    }
+    try {
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+        req.id = verifyToken.id;
+        req.name = verifyToken.name;
+        req.username = verifyToken.username;
+        next();
+    } catch (error) {
+        console.log("Catched at loginAuth: ", error);
+        res.json({error: "Login first"});
+    }
+}
+
+module.exports = loginAuth;
