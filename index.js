66e3ddad2c031b25c803977d8499f6efa2c2ead8
mdarora <mdarora0001@gmail.com>
2023-05-17 02:39:40 -0400

    react app link added for origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://public-blog.onrender.com/');
     res.setHeader('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
