bd56a2807e8e6067b0a2f3dc922b7640abeb47eb
MD Arora <40023343+mdarora@users.noreply.github.com>
2023-08-05 17:25:55 -0400

    origin set in use of cors

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ const app = express();
 require('dotenv').config();
 
 
-app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', 'https://public-blog.onrender.com/');
-    res.setHeader('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+// app.use((req, res, next) =>{
+//     res.setHeader('Access-Control-Allow-Origin', 'https://public-blog.onrender.com/');
+//     res.setHeader('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
 app.get("/", (req, res) => {
     res.json({message: "home route of server"});
 });
 
-app.use(cors());
+app.use(cors({
+    origin: "https://public-blog.onrender.com"
+}));
 app.use(express.json());
 app.use(require('cookie-parser')());
 
