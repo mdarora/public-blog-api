14d868822dca603265509e99ecc031bfd1fb3323
MD Arora <40023343+mdarora@users.noreply.github.com>
2023-08-06 13:41:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 require('dotenv').config();
 
 
-// app.use((req, res, next) =>{
-//     res.setHeader('Access-Control-Allow-Origin', 'https://public-blog.onrender.com/');
-//     res.setHeader('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     next();
-// });
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', 'https://public-blog.onrender.com/');
+    // res.setHeader('Access-Control-Allow-Methods', ' GET, POST, PUT, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 app.get("/", (req, res) => {
     res.json({message: "home route of server"});
